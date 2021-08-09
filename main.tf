098e16e0da5fa4258a801eb96d1d209df9c1fcf3
rajpal151 <81980197+rajpal151@users.noreply.github.com>
2021-08-09 22:52:16 +0530

    second change

--- main.tf
+++ main.tf
@@ -45,11 +45,16 @@ resource "aws_security_group" "MyLab_Sec_Grp" {
     name = "MyLab Security Group"
     description = "To allow inbound and outbound traffic"
     vpc_id = aws_vpc.MyLab-VPC.id
-    ingress{
-        from_port = 22
-        to_port = 22
+    dynamic ingress{
+        iterator = port
+        for_each = var.ports                //fetching  ports from variables.tf
+        content {
+        from_port = port.value
+        to_port = port.value 
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] 
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+         
     }
     egress{
         from_port = 0
@@ -61,4 +66,37 @@ resource "aws_security_group" "MyLab_Sec_Grp" {
         Name = "Allow Traffic"
     }
     
+}
+
+# Create Route table and associate it with subnet 
+resource "aws_route_table" "MyLab_RouteTable" {
+    vpc_id = aws_vpc.MyLab-VPC.id
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.MyIGW.id
+    }
+    tags = {
+        Name = "MyRouteTable"
+}
+}
+
+#create association
+resource "aws_route_table_association" "MyLab_Association" {
+    subnet_id = aws_subnet.MyLab-Subnet.id
+    route_table_id = aws_route_table.MyLab_RouteTable.id
+}
+
+# Create EC2 Instances
+resource "aws_instance" "MyNewInstance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = "workingKeyPair"
+    vpc_security_group_ids = [aws_security_group.MyLab_Sec_Grp.id]
+    subnet_id = aws_subnet.MyLab-Subnet.id
+    associate_public_ip_address = true
+
+    tags = {
+        Name = "MyNewInstance"
+    }
+
 }
